81d2d6d4a72fce60e21a6dac8537f9cf30a7cc3a
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-19 14:08:03 -0400

    Added reek support.

--- rakefile.rb
+++ rakefile.rb
@@ -8,6 +8,11 @@ Rake::TestTask.new do |t|
   t.verbose    = false
 end
 
+desc "Run a scan for smelly code!"
+task :reek do |t|
+  `reek --no-color lib > reek.txt`
+end
+
 desc "Fire up an IRB session with safe_clone preloaded."
 task :console do
   system "ruby irbt.rb local"
